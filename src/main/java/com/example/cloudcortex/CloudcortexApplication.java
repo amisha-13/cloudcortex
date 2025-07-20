d4599fe248bcd30c305cd27621c9e11a444d64e4
amisha-13 <amishajoshi2713@gmail.com>
2025-07-21 04:44:56 +0530

    Code refactor

---
+++ src/main/java/com/example/cloudcortex/CloudcortexApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudcortex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudcortexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudcortexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloudcortex/CloudcortexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudcortex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudcortexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
